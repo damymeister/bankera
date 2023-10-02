a25b52218b8afd24705061de01e8dc77281a667f
damymeister <mattiadamczyk10@gmail.com>
2023-10-02 19:09:44 +0200

    two sample interfaces

---
+++ pages/api/interfaces/post.tsx
@@ -0,0 +1,8 @@
+interface IPost {
+    id:number,
+    post_title: String,
+    post_content: String,
+    posted_on: Date,
+    user_id: number,
+  }
+  
---
+++ pages/api/interfaces/user.tsx
@@ -0,0 +1,12 @@
+interface IUser {
+    id?: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    phone_number?: string;
+    wallet_id?: number;
+    forex_wallet_id?: number;
+    account_created_on: Date;
+    role_id?: number;
+  }
