f7103d7a782df1acea819a94128724a524329ced
RaViii1 <jakub2baran@gmail.com>
2023-10-13 17:35:34 +0200

    Added css to wallet and walletModal

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -99,7 +99,7 @@ const mapUserCurrencies = () => {
   if (!isLoading && walletData !== null && currencies !== null) {
     return (
       <div>
-          <table className='w-full py-4 m-4 borderLightY text-white'>
+          <table className='w-full py-6 m-4 borderLightY text-white border-spacing-y-3' >
             <thead>
               <tr>
                 <th>Currency</th>
@@ -107,10 +107,10 @@ const mapUserCurrencies = () => {
                 <th>Action</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className='py-2'>
               {walletData.map((currency) => {
                 return (
-                  <tr key={currency.id}>
+                  <tr className='' key={currency.id}>
                     <td>{findCurrencyName(currency.currency_id)}</td>
                     <td>{currency.amount}</td>
                     <td className='flex items-center justify-center'>
@@ -131,7 +131,7 @@ const mapUserCurrencies = () => {
 
   return (
     <Layout>
-      <div className="containerCustom borderLightY">
+      <div className="containerCustom borderLightY text-white">
       <div className="items-center bg-[#1f1b24b2] justify-center flex h-full w-full">
         {isLoading ? (<div>Is loading...</div>):
         walletID === null ?
@@ -142,11 +142,11 @@ const mapUserCurrencies = () => {
           <div className=" p-8 borderLight rounded-xl m-8 containerCustom min-h-400">
             {!walletData || walletData.length === 0 ? (
            <button onClick={() => {handleDeleteWallet(walletID).then(() => window.location.reload()) }} className="w-3/5 px-4 py-2 bg-[#ff0000c0] hover:bg-[#5c2121] text-[white] rounded-md">Delete Wallet</button>) : null }
-           <h1 className='text-2xl'>Hello { userData.firstName } { userData.surname }</h1>
+           <h1 className='text-2xl   border-[#BB86FC] border-b-2 py-2 my-4'>Hello { userData.firstName } { userData.surname }!</h1>
            <p>Current balance of you account is shown below</p>
            { mapUserCurrencies() }
           { currenciesToSend.length !== 0 ? (
-          <button className='button2' onClick={() => {setShowWalletModal(true)}}>Deposit new currency into your wallet</button>
+          <button className='button3' onClick={() => {setShowWalletModal(true)}}>Deposit new currency</button>
           ):(<div>
               <p>You have all of the possibile currencies in your wallet.</p>
             </div>)}
