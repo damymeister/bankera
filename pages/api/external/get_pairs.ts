42968b6d44c8f22ebcd335b00d345241473863ac
DingoMC <martin701-2001@wp.pl>
2023-11-05 20:17:11 +0100

    Delete get_pairs endpoint

--- pages/api/external/get_pairs.ts
+++
@@ -1,7 +0,0 @@
-import getCurrencyPairs from "@/lib/currency_rates";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const data = await getCurrencyPairs()
-    return res.status(200).json(data)
-}
