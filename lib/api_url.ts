55c194ed8222cf6a89ff06efaf18ac9138564833
DingoMC <martin701-2001@wp.pl>
2023-11-08 21:05:39 +0100

    Fix server and api_url

--- lib/api_url.ts
+++ lib/api_url.ts
@@ -5,13 +5,16 @@
  * @example
  * api_url('login') returns 'http://localhost:3000/api/login'
  * api_url('auth/user') returns 'http://localhost:3000/api/auth/user'
- * @description
- * You can also set variables in .env such as:
- * NEXT_PUBLIC_PORT to define specific port,
- * NEXT_PUBLIC_API to define whole API link
  */
 export default function api_url (endpoint: string) {
-    if (process.env.NEXT_PUBLIC_API !== undefined) return process.env.NEXT_PUBLIC_API + endpoint
-    if (process.env.NEXT_PUBLIC_PORT !== undefined) return 'http://localhost:' + process.env.NEXT_PUBLIC_PORT + '/api/' + endpoint
-    return 'http://localhost:3000/api/' + endpoint
+    return hostnamePrefix() + endpoint
+}
+
+const hostnamePrefix = () => {
+    if (process.env.NEXT_PUBLIC_NET_TYPE !== undefined) {
+        if (process.env.NEXT_PUBLIC_NET_TYPE === 'LOCAL' && process.env.NEXT_PUBLIC_LOCAL_API) return process.env.NEXT_PUBLIC_LOCAL_API
+        if (process.env.NEXT_PUBLIC_NET_TYPE === 'REMOTE' && process.env.NEXT_PUBLIC_REMOTE_API) return process.env.NEXT_PUBLIC_REMOTE_API
+    }
+    if (process.env.NEXT_PUBLIC_PORT !== undefined) return 'http://localhost:' + process.env.NEXT_PUBLIC_PORT + '/api/'
+    return 'http://localhost:3000/api/'
 }
