ee7f605f924c12cc2a353096a440e8bf14181fe2
DingoMC <martin701-2001@wp.pl>
2023-11-14 14:25:04 +0100

    Add cron for currency history

--- cron.ts
+++ cron.ts
@@ -1,14 +1,15 @@
 import { Cron } from "cron-async";
 import * as updateRates from './lib/crons/updateRates'
+import * as updateHistory from './lib/crons/updateHistory'
 
 export const startCron = () => {
     const cron = new Cron()
-    let crons = [updateRates]
+    let crons = [updateRates, updateHistory]
     for (let i = 0; i < crons.length; i++) {
         cron.createJob(crons[i].name, {
             cron: crons[i].schedule,
             onTick: async () => {
-                await updateRates.run()
+                await crons[i].run()
             }
         })
     }
