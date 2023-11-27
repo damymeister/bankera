b5023e23310eafd9960e769811ea8cdaa65eeb6b
RaViii1 <jakub2baran@gmail.com>
2023-11-27 16:15:56 +0100

    Testing Chart.js

--- components/newchart.tsx
+++ components/newchart.tsx
@@ -12,8 +12,7 @@ export default function ChartExample() {
   const [currencyHistory, setCurrencyHistory] = useState<ICurrencyHistory[]>([]);
   const [sellingCurrency, setSellingCurrency] = useState<ICurrency>(allSelector)
   const [buyingCurrency, setBuyingCurrency] = useState<ICurrency>(allSelector)
-  const [pastTimestamp, setPastTimestamp] = useState('6d')
-
+  const [pastTimestamp, setPastTimestamp] = useState('6h')
 
   useEffect(() => {
     const handleGetCurrencies = async () => {
@@ -29,65 +28,81 @@ export default function ChartExample() {
     }
   }, [currencies])
 
-
   useEffect(() => {
     const handleHistoryGet = async () => {
-      
-      let url = api_url('auth/currencyHistory') + '?timestamp=' + pastTimestamp + '&includeDiff=false&invert=false&sell_currency_id=' + "118"
+      let url = api_url('auth/currencyHistory') + '?timestamp=' + pastTimestamp + '&includeDiff=false&invert=false&sell_currency_id=' + "118" + '&buy_currency_id=' + "150"
       // if (buyingCurrency.id !== -1) {
-        url += '&buy_currency_id=' + "150"
+      //   url += '&buy_currency_id=' + "150"
+      // }
       const { data } = await axios.get(url, {headers: {Accept: 'application/json'}})
       console.log(data)
       setCurrencyHistory(data as ICurrencyHistory[])
     }
-    // if (sellingCurrency.id !== -1) {
+    if (sellingCurrency.id !== -1) {
       handleHistoryGet()
-    // }
-  }, [])
+    }
+  }, [sellingCurrency, buyingCurrency, pastTimestamp])
 
   const chartRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (currencyHistory.length > 0 && chartRef.current) {
-
-      const labels = currencyHistory.flatMap((currency) =>
-        currency.history.map((historyItem) => historyItem.date)
-      );
-      const data = currencyHistory.flatMap((currency) =>
-        currency.history.map((historyItem) => historyItem.conversion_value)
-      );
-      // console.log(currencyHistory);
-      // console.log(data);
-      // console.log(labels);
-      new Chart(chartRef.current, {
-        type: "line",
-        data: {
-          labels: labels, 
-          datasets: [
-            {
-              label: "Currency Value",
-              data: data,
-              fill: false,
-              borderColor: "rgb(75, 192, 192)",
+  if (currencyHistory.length > 0 && chartRef.current) {
+    const labels = currencyHistory.map((currency) =>
+      currency.history.map((historyItem) =>
+        new Date(historyItem.date).toLocaleString(undefined, {
+          minute: "numeric",
+          hour: "numeric",
+          day: "numeric",
+          month: "numeric",
+          year: "numeric",
+        })
+      )
+    ).flat();
+    const data = currencyHistory.map((currency) =>
+      currency.history.map((historyItem) => historyItem.conversion_value)
+    ).flat();
+    new Chart(chartRef.current, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Currency Value",
+            data: data,
+            fill: false,
+            borderColor: "rgb(75, 192, 192)",
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: "Date",
             },
-          ],
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: "Currency Value",
+            },
+            ticks: {
+              precision: 8, // Set the number of decimal places for the Y-axis values
+            },
+          },
         },
-        options: {
-          scales: {
-            x: {
-              type: "time",
-            }
-          }
-        }
-      
-
-      });
-    }
-  }, [currencyHistory]);
+      },
+    });
+  }
+}, [currencyHistory]);
 
   return (
-    <div style={{ width: '400', height: '400px'}}>
-      <canvas ref={chartRef} width="1920" height="400"></canvas>
+    <div >
+      <canvas ref={chartRef} width="1820" height="800"></canvas>
     </div>
   );
 }
