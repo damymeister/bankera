f3514a65155f9458391dae6ab493432ec35d5525
DingoMC <martin701-2001@wp.pl>
2023-11-30 23:34:15 +0100

    Refactorize

--- pages/api/auth/innerTransaction.ts
+++ pages/api/auth/innerTransaction.ts
@@ -4,9 +4,17 @@ import { getCookie } from 'cookies-next';
 import parseJwt from '@/lib/parse_jwt';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const token = getCookie('token', { req, res })
+  if (!token) return res.status(401).json({error: 'Not Authorized! User Token does not exist!'})
+  const token_json = parseJwt(token)
+  const user_id = parseInt(token_json._id)
+  const user = await prisma.user.findUnique({where: {id: user_id}})
+  if (user === null) return res.status(404).json({ error: 'User not found!' })
+  const wallet_id = user.wallet_id
+  if (wallet_id === null) return res.status(404).json({ error: 'Wallet not found!' })
   if (req.method === 'GET') {
     try {
-      const response =  await prisma.inner_Transaction.findMany();
+      const response = await prisma.inner_Transaction.findMany({ where: {wallet_id: wallet_id}});
       return res.status(200).json(response); 
     } catch (error) {
       console.error('Error while managing request', error);
@@ -14,35 +22,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
   }
   if (req.method === 'POST') {
-    try{
-    let token = getCookie('token', { req, res });
-
-    if (token !== undefined) {
-      let token_json = parseJwt(token);
-      let user_id = parseInt(token_json._id);
-
-      const userWallet = await prisma.user.findUnique({
-        where: {
-          id: user_id,
-        },
-      });
-
-      if(!userWallet?.wallet_id || userWallet?.wallet_id !== parseInt(req.body.wallet_id)){
-        return res.status(404).json({ error: 'User does not have a correct wallet.' });
-      }
-
+    try {
       await prisma.inner_Transaction.create({data: {
-          wallet_id: parseInt(req.body.wallet_id),
-          currency_pair_id: parseInt(req.body.currency_pair_id),
-          inital_amount: parseFloat(req.body.initial_amount),
-          converted_amount: parseFloat(req.body.converted_amount),
-          transaction_date: new Date(req.body.transaction_date)
+        wallet_id: wallet_id,
+        currency_pair_id: parseInt(req.body.currency_pair_id),
+        inital_amount: parseFloat(req.body.initial_amount),
+        converted_amount: parseFloat(req.body.converted_amount),
+        transaction_date: new Date(req.body.transaction_date)
       }})
-      
-    return res.status(201).json({ message: "Currency exchange completed successfully." })
-  }
-    } catch(error){
+      return res.status(201).json({ message: "Currency exchange completed successfully." })
+    } catch(error) {
       return res.status(500).json({ message: 'Error while trying to exchange currencies.' });
     }
   }
+  return res.status(500).json({message: "This HTTP method is not supported on this endpoint"})
 }  
