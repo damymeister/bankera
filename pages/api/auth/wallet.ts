024ef6ddf5e7e0427d33ae78af5481e0b9ba2f99
DingoMC <martin701-2001@wp.pl>
2023-11-30 23:51:33 +0100

    Refactorize

--- pages/api/auth/wallet.ts
+++ pages/api/auth/wallet.ts
@@ -4,31 +4,21 @@ import { getCookie } from 'cookies-next';
 import parseJwt from '@/lib/parse_jwt';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const token = getCookie('token', { req, res })
+  if (!token) return res.status(401).json({error: 'Not Authorized! User Token does not exist!'})
+  const token_json = parseJwt(token)
+  const user_id = parseInt(token_json._id)
+  const user = await prisma.user.findUnique({where: {id: user_id}})
+  if (user === null) return res.status(404).json({ error: 'User not found!' })
+  const wallet_id = user.wallet_id
   if (req.method === 'GET') {
     try {
-      let token = getCookie('token', { req, res });
-      if (token !== undefined) {
-        let token_json = parseJwt(token);
-        let user_id = parseInt(token_json._id);
-
-        const userWallet = await prisma.user.findUnique({
-          where: {
-            id: user_id,
-          },
-        });
-
-        if (!userWallet) {
-          return res.status(404).json({ error: 'User not found.' });
-        }
-
-        if (!userWallet.wallet_id) {
-          return res.status(404).json({ error: 'User does not have specified wallet ID.' });
-        }
-        
-         return res.status(200).json({wallet_id: userWallet.wallet_id, first_name: userWallet.first_name, last_name: userWallet.last_name });
-      
-      } 
-      return res.status(401).json({ error: 'Permission denied. User is not authenticated.' });
+      if (!wallet_id) return res.status(404).json({error: 'User does not have a wallet!'})
+      const wallet = await prisma.wallet.findUnique({
+        where: { id: wallet_id }
+      })
+      if (!wallet) return res.status(404).json({error: 'Wallet not found'})
+      return res.status(200).json({wallet_id: user.wallet_id, first_name: user.first_name, last_name: user.last_name });
     } catch (error) {
       console.error('Error while managing request', error);
       return res.status(500).json({ error: 'Server error occured.' });
@@ -36,75 +26,51 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } 
   if (req.method === 'POST') {
     try {
-      let token = getCookie('token', { req, res });
-      if (token !== undefined) {
-        let token_json = parseJwt(token);
-        let user_id = parseInt(token_json._id);
-        const findUser = await prisma.user.findUnique({
-          where: {
-            id: user_id,
-          },
-        });
-
-        if(findUser?.wallet_id){
-          return res.status(404).json({ error: 'User can only have one wallet.' });
-        }
-
-        const createdWallet = await prisma.wallet.create({ data: { created_on: new Date()} });
-        if(!createdWallet){
-            return res.status(404).json({ error: 'Error while creating wallet.' });
-        }
-        const walletId = createdWallet.id;
-        const updatedUser = await prisma.user.update({
-            where: {
-              id: user_id, 
-            },
-            data: {
-              wallet_id: walletId, 
-            }});
-            
-        if(!updatedUser){
-            return res.status(404).json({ error: 'Error while assigning User to Wallet.' });
-        }
-        return res.status(200).json({ message: "Wallet Created Successfully", wallet_id : walletId });
+      if (user.wallet_id) {
+        return res.status(404).json({ error: 'User can only have one wallet.' });
       }
-      return res.status(500).json({ message: "Error while creating Wallet" });
+      const createdWallet = await prisma.wallet.create({ data: { created_on: new Date()} });
+      if (!createdWallet) {
+        return res.status(404).json({ error: 'Error while creating wallet.' });
+      }
+      const new_wallet_id = createdWallet.id;
+      const updatedUser = await prisma.user.update({
+        where: {
+          id: user_id, 
+        },
+        data: {
+          wallet_id: new_wallet_id, 
+        }
+      });
+      if (!updatedUser) {
+        return res.status(404).json({ error: 'Error while assigning User to Wallet.' });
+      }
+      return res.status(200).json({ message: "Wallet Created Successfully", wallet_id: new_wallet_id });
     } catch (error) {
       console.error('Error while managing POST method', error);
       return res.status(500).json({ error: 'Error while creating wallet.' });
     }
   } 
   if (req.method === 'DELETE') {
-  let token = getCookie('token', { req, res });
-    if (token !== undefined) {
-      try {
-        let token_json = parseJwt(token);
-        let user_id = parseInt(token_json._id);
-        const user = await prisma.user.findUnique({where: {id: user_id}})
-        const wallet_id = user?.wallet_id
-
-        await prisma.user.update({
-          where: {
-            id: user_id
-          },
-          data: {
-            wallet_id: null
-          }
-        });
-        
-        if (wallet_id && wallet_id !== null) {
-          await prisma.wallet.delete({
-            where: {id: wallet_id}, 
-          });
+    try {
+      await prisma.user.update({
+        where: {
+          id: user_id
+        },
+        data: {
+          wallet_id: null
         }
-
-
-        return res.status(200).json({ message: "Wallet has been deleted" });
-      } catch (error) {
-        console.error('Error while deleting wallet', error);
-        return res.status(500).json({ error: 'Server error occurred.' });
+      });
+      if (wallet_id !== null) {
+        await prisma.wallet.delete({
+          where: {id: wallet_id}, 
+        });
       }
-    } 
-    return res.status(401).json({ error: 'Permission denied. User is not authenticated.' });
+      return res.status(200).json({ message: "Wallet has been deleted" });
+    } catch (error) {
+      console.error('Error while deleting wallet', error);
+      return res.status(500).json({ error: 'Server error occurred.' });
+    }
   }
+  return res.status(500).json({message: "This HTTP method is not supported on this endpoint"})
 }
