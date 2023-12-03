ced2a23db362fef8e0b99894579e0916a292dd39
DingoMC <martin701-2001@wp.pl>
2023-12-03 12:36:47 +0100

    Add currency invertion

--- components/newchart.tsx
+++ components/newchart.tsx
@@ -207,7 +207,10 @@ export default function ChartExample() {
                   })
                 }
             </select>
-            <FaExchangeAlt />
+            <FaExchangeAlt onClick={() => {
+              setSellingCurrency(buyingCurrency)
+              setBuyingCurrency(sellingCurrency)
+            }} />
             <select className="bgdark border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 name="buy_currency"
                 id="buy_currency"
