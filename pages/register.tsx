7cf4a0b5a49885bbb109ed7b56906fa040585e0f
DingoMC <martin701-2001@wp.pl>
2023-12-07 17:10:47 +0100

    Minor fixes on snackbar

--- pages/register.tsx
+++ pages/register.tsx
@@ -9,13 +9,23 @@ import { PrismaClient } from "@prisma/client";
 import api_url from '@/lib/api_url';
 import axios from 'axios'
 import { useRouter } from 'next/router';
+import { FaExclamation } from 'react-icons/fa';
+import SnackBar from '@/components/snackbar';
 
 
 export default function Register() {
     const router = useRouter()
     const [data, setData] = useState({first_name: '', last_name: '', email: '', password: '', phone_number: ''})
     const [error, setError] = useState('')
-  
+    const [showSnackbar, setShowSnackbar] = useState(false)
+    const [snackMess, setsnackMess] = useState("Błąd w trakcie rejestracji")
+    const [snackStatus, setsnackStatus] = useState("danger")
+    const snackbarProps = {
+      status: snackStatus,
+      icon: <FaExclamation />,
+      description: snackMess
+    };
+
     const handleRegister = async (e: React.SyntheticEvent) => {
       e.preventDefault();
       try {
@@ -31,6 +41,7 @@ export default function Register() {
               error.response.status <= 500
           ) {
               setError(error.response.data.message)
+              setShowSnackbar(true)
           }
         }
         else console.log('unexpected error: ', error)
@@ -53,8 +64,9 @@ export default function Register() {
 
     return (
         <Layout>
+            {showSnackbar && <SnackBar snackbar={snackbarProps} setShowSnackbar={setShowSnackbar} />}
             <div className="formContainer">
-                <form onSubmit={handleRegister}>
+                <form className="frame" onSubmit={handleRegister}>
                     <h2>Utwórz konto</h2>
 
                     <div className="inputs">
